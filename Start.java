9ef9a476cec3177c52899f2569682cb194dabcae
Anastasya <blum1098@yandex.ru>
2022-08-21 22:53:11 +0300

    deleted filse

--- Start.java
+++
@@ -1,9 +0,0 @@
-public class Start {
-    public static void main (String[] args) {
-        int price = 5300;
-        int w = 20;
-        int mile = price/w ;
-
-        System.out.println(mile);
-    }
-}
